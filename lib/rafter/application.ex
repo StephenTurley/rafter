fe6851f16ac3cfa77a66157e9b043bad1a6ab90a
Stephen Turley <stephen@turley.dev>
2021-11-21 09:00:54 -0500

    initial commit

---
+++ lib/rafter/application.ex
@@ -0,0 +1,9 @@
+defmodule Rafter.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = []
+    opts = [strategy: :one_for_one, name: Rafter.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ test/rafter_test.exs
@@ -0,0 +1,8 @@
+defmodule RafterTest do
+  use ExUnit.Case
+  doctest Rafter
+
+  test "greets the world" do
+    assert Rafter.hello() == :world
+  end
+end
