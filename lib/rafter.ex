5c13ef20d59d54c0ad578c5e73774ba87cc813e1
Stephen Turley <stephen@turley.dev>
2021-11-26 15:15:19 -0500

    added append_entries to Consensus

--- lib/rafter.ex
+++ lib/rafter.ex
@@ -2,4 +2,8 @@ defmodule Rafter do
   @moduledoc """
   Documentation for `Rafter`.
   """
+
+  def append_entry(entry) do
+    Rafter.Consensus.append_entry(entry)
+  end
 end
--- lib/rafter/consensus.ex
+++ lib/rafter/consensus.ex
@@ -2,13 +2,31 @@ defmodule Rafter.Consensus do
   require Logger
   use GenServer
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts)
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   @impl true
   def init(_args) do
     Logger.info("Consensus module has started.")
-    {:ok, %{}}
+
+    state = %{
+      log: []
+    }
+
+    {:ok, state}
+  end
+
+  # Server
+  @impl true
+  def handle_call({:append_entries, entries}, _from, %{log: log} = state) do
+    Logger.info("Entries added")
+
+    {:reply, :ok, Map.replace!(state, :log, entries ++ log)}
+  end
+
+  # Client
+  def append_entry(entry) do
+    GenServer.call(__MODULE__, {:append_entries, [entry]})
   end
 end
